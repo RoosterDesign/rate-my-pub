1a00090ada92a5c01d64b1cb9a6ad6e8f0d03a2b
Neil <neil.dewing@happydance.love>
2026-05-08 16:42:11 +0100

    additional criteria

--- sql/seed.sql
+++ sql/seed.sql
@@ -4,5 +4,9 @@ INSERT INTO criteria (name, subtitle, display_order) VALUES
   ('General Ambience', NULL, 3),
   ('Outdoor Space', NULL, 4),
   ('Entertainment', 'Music, Live Sports, Pool table, Dartboard etc...', 5),
-  ('Staff', 'Friendliness, speedy service, etc...', 6)
+  ('Staff', 'Friendliness, speedy service, etc...', 6),
+  ('Toilets', 'Cleanliness, stupid door signs, etc...', 7),
+  ('Radiators', NULL, 8),
+  ('View', 'The view from inside or sat outside the pub', 9),
+  ('Bike Facilities', 'Somewhere safe to lock up your bike, or dedicated bike storage', 10)
 ON CONFLICT DO NOTHING;
